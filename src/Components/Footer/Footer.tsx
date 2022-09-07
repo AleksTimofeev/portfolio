8fe6106a97d573c4e728896692a05519642d5e16
AleksTimofeev <timoha144a@gmail.com>
2022-09-07 22:54:06 +0300

    change footer

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import styles from './Footer.module.scss'
+import linkedinLogo from './../../Assets/Image/linkedin-logo.png'
+import telegramLogo from './../../Assets/Image/telegram-logo.png'
+import gitLogo from './../../Assets/Image/git-logo.png'
 
 const Footer = () => {
   return (
@@ -7,10 +10,15 @@ const Footer = () => {
       <div className={styles.container}>
         <h2 className={styles.title}>Aleksandr Timofeev</h2>
         <div className={styles.list}>
-          <div className={styles.listItem}>001</div>
-          <div className={styles.listItem}>002</div>
-          <div className={styles.listItem}>003</div>
-          <div className={styles.listItem}>004</div>
+          <div className={styles.listItem}>
+            <img height={'50px'} src={gitLogo} alt={'git logo'}/>
+          </div>
+          <div className={styles.listItem}>
+            <img height={'50px'} src={linkedinLogo} alt={'linkedin logo'}/>
+          </div>
+          <div className={styles.listItem}>
+            <img height={'50px'} src={telegramLogo} alt={'telegram logo'} />
+          </div>
         </div>
       </div>
     </footer>
