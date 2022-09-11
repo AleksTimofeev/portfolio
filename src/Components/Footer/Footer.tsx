b0d18efd3784d532b7579cc560e36534dd2beafe
AleksTimofeev <timoha144a@gmail.com>
2022-09-11 23:19:20 +0300

    ...

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -11,13 +11,21 @@ const Footer = () => {
         <h2 className={styles.title}>Aleksandr Timofeev</h2>
         <div className={styles.list}>
           <div className={styles.listItem}>
-            <img height={'50px'} src={gitLogo} alt={'git logo'}/>
+            <a href={'https://github.com/AleksTimofeev'} target={'_blank'} rel={'noreferrer'}>
+              <img height={'50px'} src={gitLogo} alt={'git logo'}/>
+            </a>
           </div>
           <div className={styles.listItem}>
-            <img height={'50px'} src={linkedinLogo} alt={'linkedin logo'}/>
+            <a href={
+              'https://www.linkedin.com/in/aleksandr-timofeev-531062244/?lipi=urn%3Ali%3Apage%3Ad_flagship3_feed%3Bwc1Aw%2F90RJu0cU6TvNuSlw%3D%3D'
+            } target={'_blank'} rel={'noreferrer'}>
+              <img height={'50px'} src={linkedinLogo} alt={'linkedin logo'}/>
+            </a>
           </div>
           <div className={styles.listItem}>
-            <img height={'50px'} src={telegramLogo} alt={'telegram logo'} />
+            <a href={'https://t.me/TimofeevAleks'}>
+              <img height={'50px'} src={telegramLogo} alt={'telegram logo'}/>
+            </a>
           </div>
         </div>
       </div>
