6aacdb7763f12f5987a0692fe6b3fa7f03537912
AleksTimofeev <timoha144a@gmail.com>
2023-03-07 00:18:20 +0300

    change url send message

--- src/api/SendMessage.ts
+++ src/api/SendMessage.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 
 export const sendMessage = async (data: DataMessageType) => {
   try {
-    const res = await axios.post('http://localhost:3000/new-email', data)
+    const res = await axios.post('https://portfolio-back-teal.vercel.app/new-email', data)
     return res.data
   }catch (e){
     console.log(e)
